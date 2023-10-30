2fa6e12176b583daea88e213115819b5b0eea084
Damien <damiensabo@live.fr>
2023-10-30 15:35:51 +0100

    Add links in ListGroupItems

--- src/components/VideoProjectItem/VideoProjectItem.jsx
+++ src/components/VideoProjectItem/VideoProjectItem.jsx
@@ -4,22 +4,24 @@ import {ListGroup, Badge} from "react-bootstrap";
 export default function VideoProjectItem({item}) {
   return (
     <>
-      <ListGroup.Item as="li" className="VideoProjectItem">
-        <div className="VideoProjectItem--title-container">
-          <div className="VideoProjectItem--title font-bold">
-            <span>{item.name}</span>
+      <ListGroup.Item as="a" className="VideoProjectItem" href={`/projects/${item.id}`}>
+        <li>
+          <div className="VideoProjectItem--title-container">
+            <div className="VideoProjectItem--title font-bold">
+              <span>{item.name}</span>
+            </div>
+            <div className="VideoProjectItem--dates small font-secondary">
+              created on {new Date(item.creationDate).toLocaleDateString()}
+            </div>
           </div>
-          <div className="VideoProjectItem--dates small font-secondary">
-            created on {new Date(item.creationDate).toLocaleDateString()}
+          <div className="VideoProjectItem--author small">
+            <Badge>{item.author}</Badge>
+            &nbsp;<span className="font-secondary">{item.type}</span>
           </div>
-        </div>
-        <div className="VideoProjectItem--author small">
-          <Badge>{item.author}</Badge>
-          &nbsp;<span className="font-secondary">{item.type}</span>
-        </div>
-        <div className="VideoProjectItem--description">
-          {item.description}
-        </div>
+          <div className="VideoProjectItem--description">
+            {item.description}
+          </div>
+        </li>
       </ListGroup.Item>
     </>
   );
